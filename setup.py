c7aa76e62c1bddee376dcaeb4b54f223ffd90159
PouncySilverkitten <struan@duncan-wilson.co.uk>
2018-03-17 22:08:16 +0000

    Add setup.py

--- setup.py
+++ setup.py
@@ -1,4 +1,14 @@
+from distutils.core import setup
+
+
 setup(
+    name='pylint-badge'.
+    version='0.9',
+    description="Runs pylint to generate badges",
+    author="Pouncy Silverkitten",
+    author_email="pouncy.sk@gmail.com",
+    url="https://github.com/pouncysilverkitten/pylint-badge",
+    install_requires=["pylint",]
     entry_points = {
         'console_scripts': ['pylint-badge=pylint-badge.pylint-badge:main'],
     }
